d3d816f3bde08997bba0b455407620742278576c
AdilhanKaikenov <adilhan_kai@mail.ru>
2016-11-18 20:31:22 +0600

    Add abstract class BaseEntity

---
+++ src/main/java/com/epam/adk/web/library/model/BaseEntity.java
@@ -0,0 +1,42 @@
+package com.epam.adk.web.library.model;
+
+/**
+ * Abstract class BaseEntity created on 18.11.2016
+ *
+ * @author Kaikenov Adilhan
+ */
+public abstract class BaseEntity {
+
+    private Integer id;
+
+    public BaseEntity() {
+    }
+
+    public BaseEntity(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BaseEntity that = (BaseEntity) o;
+
+        return id != null ? id.equals(that.id) : that.id == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
