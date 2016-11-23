eb1f545effcf95c92d6519d3eb9a77a2748f3755
AdilhanKaikenov <adilhan_kai@mail.ru>
2016-11-23 21:16:15 +0600

    Add FrontControllerServlet initial commit

---
+++ src/main/java/com/epam/adk/web/library/servlet/FrontControllerServlet.java
@@ -0,0 +1,32 @@
+package com.epam.adk.web.library.servlet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * FrontControllerServlet class created on 18.11.2016
+ *
+ * @author Kaikenov Adilhan
+ */
+public class FrontControllerServlet extends HttpServlet {
+
+    private static final Logger log = LoggerFactory.getLogger(FrontControllerServlet.class);
+
+    @Override
+    public void init() throws ServletException {
+
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+    }
+}
