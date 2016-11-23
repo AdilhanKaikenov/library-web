31e1e5bd6d5cae5f49a4add404f44de0396e374e
AdilhanKaikenov <adilhan_kai@mail.ru>
2016-11-23 23:35:13 +0600

    Add CharsetFilter & update web.xml

---
+++ src/main/java/com/epam/adk/web/library/filter/CharsetFilter.java
@@ -0,0 +1,35 @@
+package com.epam.adk.web.library.filter;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * CharsetFilter class created on 23.11.2016
+ *
+ * @author Kaikenov Adilhan
+ * @see Filter
+ */
+public class CharsetFilter implements Filter {
+
+    private static final Logger log = LoggerFactory.getLogger(CharsetFilter.class);
+    private static final String ENCODING = "UTF-8";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.debug("The CharsetFilter is initialized");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding(ENCODING);
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
