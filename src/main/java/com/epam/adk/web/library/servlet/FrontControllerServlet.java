e078e57958ce76d5641228789b94bfcf7880243c
AdilhanKaikenov <adilhan_kai@mail.ru>
2016-11-24 23:13:32 +0600

    Update FrontControllerServlet

--- src/main/java/com/epam/adk/web/library/servlet/FrontControllerServlet.java
+++ src/main/java/com/epam/adk/web/library/servlet/FrontControllerServlet.java
@@ -1,9 +1,10 @@
 package com.epam.adk.web.library.servlet;
 
+import com.epam.adk.web.library.action.Action;
+import com.epam.adk.web.library.action.ActionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,15 +19,43 @@ import java.io.IOException;
 public class FrontControllerServlet extends HttpServlet {
 
     private static final Logger log = LoggerFactory.getLogger(FrontControllerServlet.class);
-
-    @Override
-    public void init() throws ServletException {
-
-    }
+    private static final String REDIRECT_PREFIX = "redirect:";
+    private static final ActionFactory factory = ActionFactory.getInstance();
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Action action = factory.getAction(getActionName(request));
+        if (action == null) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        String view = action.execute(request, response);
+        proceedTo(request, response, view);
+    }
 
+    private void proceedTo(HttpServletRequest request, HttpServletResponse response, String view) throws IOException, ServletException {
+        if (view == null){
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        if (view.contains(REDIRECT_PREFIX)){
+            response.sendRedirect(getRedirectURL(request, view));
+        } else {
+            request.getRequestDispatcher("/WEB-INF/" + view + ".jsp").forward(request, response);
+        }
+    }
 
+    private String getRedirectURL(HttpServletRequest request, String view) {
+        return request.getContextPath() + "/do/?action=show-page&page=" + view.substring(REDIRECT_PREFIX.length());
+    }
+
+    /**
+     * The method to get action name from request.
+     *
+     * @param request
+     * @return String
+     */
+    private String getActionName(HttpServletRequest request) {
+        return request.getParameter("action");
     }
 }
